0708827062675f67b18a6a86d520ed1c40cc9112
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2024-06-24 12:27:48 +0200

    Theme selector (#9)
    * add theme-switch
    * add theme-switch-dependencies
    
    ---------
    
    Signed-off-by: Lemonatix <ph.mika.ma.riesterer@gmail.com>
    Co-authored-by: biosfood <leisenhau@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MainApp());
+SharedPreferences? preferences;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  preferences = await SharedPreferences.getInstance();
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  bool _isDarkMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTheme();
+  }
+
+  // Load theme preference from SharedPreferences
+  void _loadTheme() {
+    setState(() {
+      _isDarkMode = (preferences?.getBool('isDarkMode') ?? false);
+    });
+  }
+
+  // Save theme preference to SharedPreferences
+  void _saveTheme(bool value) {
+    preferences?.setBool('isDarkMode', value);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Flutter Theme Switch'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Settings'),
+              ),
+              SwitchListTile(
+                title: const Text('Dark Mode'),
+                value: _isDarkMode,
+                onChanged: (value) {
+                  setState(() {
+                    _isDarkMode = value;
+                    _saveTheme(value);
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+        body: const Center(
+          child: Text(
+            'Toggle theme using the sidebar menu.',
+            style: TextStyle(fontSize: 18),
+          ),
         ),
       ),
     );
